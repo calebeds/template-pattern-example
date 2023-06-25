3c57931dee8f99f6bd882ec0a792088cf11e5318
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-06-25 09:38:03 -0300

    add template pattern example

---
+++ src/GlassHouse.java
@@ -0,0 +1,11 @@
+public class GlassHouse extends HouseTemplate {
+    @Override
+    public void buildWalls() {
+        System.out.println("Build Glass Walls");
+    }
+
+    @Override
+    public void buildPillars() {
+        System.out.println("Build Pillars With Glass Coating");
+    }
+}
